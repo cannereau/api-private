c70fafb7864ff0a21de1c2f28f1a0b09b4727265
cannereau <c.annereau@fotlan.com>
2019-05-04 19:01:46 +0200

    include requests package

--- check.bat
+++ check.bat
@@ -1,5 +1,9 @@
 @echo off
 
+rem install requests package
+pip install requests
+
+rem use 7zip to build package
 7z a -tzip check.zip check.py
 7z a -tzip -r check.zip certifi
 7z a -tzip -r check.zip chardet
@@ -7,5 +11,8 @@
 7z a -tzip -r check.zip requests
 7z a -tzip -r check.zip urllib3
 
+rem update Lambda code with AWS CLI
 aws lambda update-function-code --function-name api-private-check --zip-file fileb://check.zip --output table
+
+rem remove package
 del check.zip
